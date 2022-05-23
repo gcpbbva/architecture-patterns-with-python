22a6edb92445855bdad8d9ffe70a86a9112cd2af
apecr <alberto.eyo@gmail.com>
2022-05-23 16:19:17 +0200

    test(test_model.py)
    Can not allocate twice

--- test_model.py
+++ test_model.py
@@ -73,3 +73,10 @@ def test_can_deallocate_when_order_reference():
     batch.deallocate(order_line=order_line)
 
     assert batch.quantity == 20
+
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
